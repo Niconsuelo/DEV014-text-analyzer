5b79ecd5b91a743e9de4d2906e59b1767413f1a2
Nicole Olave <nicoleconsuelo@Nicoles-MacBook-Pro.local>
2024-03-01 01:00:05 +0100

    test analyzer - ultimos cambios de analyzer, modificacion html, modificacion index

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,6 +1,7 @@
 const analyzer = {
-  getWordCount: (text) => {
-    //3 OK TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => { //3----[2] OK
+   
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
     let arregloPalabras = text.split(" ");
     let contadorPalabras = 0;
@@ -13,14 +14,15 @@ const analyzer = {
     }
     return contadorPalabras;
   },
-
-  getCharacterCount: (text) => {
+  getCharacterCount: (text) => { // 1 ----[0] OK
+  
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
     return text.length;
-
   },
-  getCharacterCountExcludingSpaces: (text) => {
-    //2 TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCountExcludingSpaces: (text) => { // 2 ----[1] OK
+    
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let arregloPalabras = text.split(" ");
     let SignosPuntuacion = [
       "!",
       "¡",
@@ -42,22 +44,23 @@ const analyzer = {
       "{",
       "}",
     ];
-    let arregloPalabras = text.split(" ");
-    let recuento = 0;
+    let caracteresSinespacio = 0;
 
     for (let i = 0; i < arregloPalabras.length; i++) {
-      if (!SignosPuntuacion.includes(arregloPalabras[i])) {
-        //2 Recuento sin signos ni espacios
-        recuento = recuento + 1;
+      if (arregloPalabras[i] !== "") {
+        if (!SignosPuntuacion.includes(arregloPalabras[i])) {
+          //si es falso, entra
+          caracteresSinespacio = caracteresSinespacio + arregloPalabras[i].length;
+        }
       }
+
+      //arregloPalabras[i] // haga un filtro, y me diga si es un signo o no.
     }
-    return recuento;
+    return caracteresSinespacio;
   },
   getAverageWordLength: (text) => {
-    //6 OK TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //6 TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
     let arregloPalabras = text.split(" ");
-    let contadorPalabras = 0;
-    let sumaNumeros = 0;
     let SignosPuntuacion = [
       "!",
       "¡",
@@ -79,72 +82,51 @@ const analyzer = {
       "{",
       "}",
     ];
-    let recuento = 0;
-    let contadorNumeros = 0;
-    let sumaPalabras = 0;
-    let longuitudPromedio = 0;
-
-    for (let i = 0; i < arregloPalabras.length; i++) {
-      if (arregloPalabras[i] !== "") {
-        //  3 Recuento palabras sin espacio
-        contadorPalabras = contadorPalabras + 1;
-
-        sumaPalabras = sumaPalabras + arregloPalabras[i].length; // suma de numeros
-        if (!SignosPuntuacion.includes(arregloPalabras[i])) {
-          //2 Recuento sin signos ni espacios
-          recuento = recuento + 1;
-        }
-
-        let palabraNumeros = Number(arregloPalabras[i]); // 5 suma de total de numeros NaN NaN = true //NaN !NaN = true !NaN = numero
-        if (!isNaN(palabraNumeros)) {
-          //Si no es no un numero, es un numero.
-          sumaNumeros = palabraNumeros + sumaNumeros;
-
-          contadorNumeros = contadorNumeros + 1; // 4 Recuento de numeros
-        }
-      }
-      longuitudPromedio = sumaPalabras / contadorPalabras; //6 longuitud promedio
-    }
-
-    return longuitudPromedio;
-  },
-
-  getNumberCount: (text) => {
-    //4 TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    let arregloPalabras = text.split(" ");
+    let caracteresSinespacio = 0;
     let contadorPalabras = 0;
-    let contadorNumeros = 0;
 
     for (let i = 0; i < arregloPalabras.length; i++) {
       if (arregloPalabras[i] !== "") {
         contadorPalabras = contadorPalabras + 1;
+        if (!SignosPuntuacion.includes(arregloPalabras[i])) {
+          //si es falso, entra
+          caracteresSinespacio = caracteresSinespacio + arregloPalabras[i].length;
+        }
       }
-      let palabraNumeros = Number(arregloPalabras[i]); // 5 suma de total de numeros NaN NaN = true //NaN !NaN = true !NaN = numero
-      if (!isNaN(palabraNumeros)) {
-        //Si no es no un numero, es un numero.
 
-        contadorNumeros = contadorNumeros + 1; // 4 Recuento de numeros
-      }
+      //arregloPalabras[i] // haga un filtro, y me diga si es un signo o no.
     }
-    return contadorNumeros;
-  },
-  getNumberSum: (text) => {
-    //5 TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+return caracteresSinespacio / contadorPalabras;
 
-    let sumaNumeros = 0;
+  },
+  getNumberCount: (text) => { //4 ----[3] OK
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
     let arregloPalabras = text.split(" ");
+    let totalNumeros = 0;
 
     for (let i = 0; i < arregloPalabras.length; i++) {
       if (arregloPalabras[i] !== "") {
-        let palabraNumeros = Number(arregloPalabras[i]);
-      if (!isNaN(palabraNumeros)) {
-        sumaNumeros = palabraNumeros + sumaNumeros;
+        let numero = Number(arregloPalabras[i]);
+        if (!isNaN(numero)) {
+          totalNumeros = totalNumeros + 1; 
+        }
+      }  
+    }
+    return totalNumeros;
+  },
+  getNumberSum: (text) => {// 5 --[4] OK
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let arregloPalabras = text.split(" ");
+    let sumaNumeros = 0;
+    for (let i = 0; i < arregloPalabras.length; i++) {
+      if (arregloPalabras[i] !== "") {
+        let numero = Number(arregloPalabras[i]);
+        if (!isNaN(numero)) {
+          sumaNumeros = numero + sumaNumeros;
+        }
       }
-      }
-      
     }
     return sumaNumeros;
   },
 };
-
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -3,81 +3,42 @@ import analyzer from "./analyzer.js";
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 document.addEventListener("DOMContentLoaded", function () {
-
   const boton = document.getElementById("reset-button"); // quiero que boton haga una accion, cuando se haga click
-  boton.addEventListener("click", function () {  // 
+  boton.addEventListener("click", function () {
+    //
 
     document.getElementsByName("user-input")[0].value = "";
-    //get elemento retorna cajas, al accder a la caja de ese elemento, debo poner [0], para encontrar el textarea, manipularlo 
-
-    document.getElementById("contador").innerHTML = '0';  //cuando aprete el click, aparecera un 0 por default
-    document.getElementById("palabras").innerHTML = '0';
-    document.getElementById("numero").innerHTML = '0';
-    document.getElementById("suma").innerHTML = '0';
-    document.getElementById("sinespacio").innerHTML = '0';
-    document.getElementById("promedio").innerHTML = '0';
+    //get elemento retorna cajas, al accder a la caja de ese elemento, debo poner [0], para encontrar el textarea, manipularlo
   });
-  const elementosLi = document.querySelectorAll('.elemento-caja');
-  console.log (elementosLi);
+  const elementosLi = document.querySelectorAll(".elemento-caja");
+
   const textarea = document.querySelector("[name='user-input']"); //al query busca elemento .areatexto, la variable contiene que sepa que es el area text
   const contador = elementosLi[0]; // 1
   const palabra1 = elementosLi[2]; // 3
-  const numeros = elementosLi[3]; // 4
-  const suma1 = elementosLi[4]; // 5
-  const totalPalabra = elementosLi[1]; // 2
+  const totalNumeros = elementosLi[3]; // 4
+  const sumaNumeros = elementosLi[4]; // 5
+  const caracteresSinespacio = elementosLi[1]; // 2
   const longuitud = elementosLi[5]; //6
 
-
-
-  textarea.addEventListener("input", function () { //1contador caracteres 
-    contador.innerHTML = "Caràcteres:"+ " " + analyzer.getCharacterCount(textarea.value);
+  textarea.addEventListener("input", function () {
+    //1contador caracteres
+    contador.innerHTML =
+      "Caràcteres:" + " " + analyzer.getCharacterCount(textarea.value);
     //cuando event listener escucha un evento en textarea, se activa el input, se ejecuta funcion
 
-    let text = textarea.value.trim(); // limpia el texto, solo inicio y final del texto.
-    let arregloPalabras = text.split(" "); // separa palabras, una de otras. segun el delimitador, en este caso es el espacio.
-
-    // ahora necesito que el contador de palabras, solo tome palabras y no cuente los espacios
-    // que existen entre palabras dentro del texto.
-
-    let contadorPalabras = 0;
-    let contadorNumeros = 0;
-    let sumaNumeros = 0;
-    let sumaPalabras = 0;
-    let SignosPuntuacion = ['!', '¡', '?', '¿', ',', '.', ':', ';', '...', '(', ')', '[', ']', '"', '-', 'º', '*', '{', '}'];
-    let recuento = 0;
-    let longuitudPromedio = 0;
-    let textVacio = 0;
-
-
-    for (let i = 0; i < arregloPalabras.length; i++) {
-      if (arregloPalabras[i] !== "") {  //  3 Recuento palabras sin espacio
-        contadorPalabras = contadorPalabras + 1;
-
-        sumaPalabras = sumaPalabras + arregloPalabras[i].length; // suma de numeros
-      if (!SignosPuntuacion.includes(arregloPalabras[i])){//2 Recuento sin signos ni espacios
-        recuento = recuento + 1;
-      }  
-
-        let palabraNumeros = Number(arregloPalabras[i]); // 5 suma de total de numeros NaN NaN = true //NaN !NaN = true !NaN = numero
-        if (!(isNaN(palabraNumeros))) {
-          //Si no es no un numero, es un numero.
-          sumaNumeros = palabraNumeros + sumaNumeros; 
-          
-          contadorNumeros = contadorNumeros + 1; // 4 Recuento de numeros
-        }
-      }
-      if (arregloPalabras[i] == "") {
-        return textVacio;
-      }
-    }
-    longuitudPromedio = sumaPalabras / contadorPalabras; //6 longuitud promedio
-
-    totalPalabra.innerHTML = "Carácteres sin espacios:" + " " + analyzer.getCharacterCountExcludingSpaces(textarea.value);// 2 Recuento de caracteres excluyendo espacios y signos de puntuación: la aplicación debe poder contar el número de caracteres en el texto
-    palabra1.innerHTML = "Palabras:" + " " + analyzer.getWordCount(textarea.value); // 3 recuento de palabras: la aplicación debe poder contar el número de palabras en el texto de entrada
-    numeros.innerHTML = "Números:" + " " + analyzer.getNumberCount(textarea.value);// 4 Recuento de números: la aplicación debe contar cúantos números hay en el texto de entrada
-    suma1.innerHTML = "Suma de números:"+ " " + analyzer.getNumberSum(textarea.value); // 5 Suma total de números: la aplicación debe sumar todos los números que hay en el texto de entrada 
-    longuitud.innerHTML = "Promedio Longuitud:" + " " + analyzer.getAverageWordLength(textarea.value); // 6 Longitud media de las palabras: la aplicación debe calcular la longitud media de las palabras en el texto
-    
+    caracteresSinespacio.innerHTML =
+      "Carácteres sin espacios:" +
+      " " +
+      analyzer.getCharacterCountExcludingSpaces(textarea.value); // 2 Recuento de caracteres excluyendo espacios y signos de puntuación: la aplicación debe poder contar el número de caracteres en el texto
+    palabra1.innerHTML =
+      "Palabras:" + " " + analyzer.getWordCount(textarea.value); // 3 recuento de palabras: la aplicación debe poder contar el número de palabras en el texto de entrada
+    totalNumeros.innerHTML =
+      "Números:" + " " + analyzer.getNumberCount(textarea.value); // 4 Recuento de números: la aplicación debe contar cúantos números hay en el texto de entrada
+    sumaNumeros.innerHTML =
+      "Suma de números:" + " " + analyzer.getNumberSum(textarea.value); // 5 Suma total de números: la aplicación debe sumar todos los números que hay en el texto de entrada
+    longuitud.innerHTML =
+      "Promedio Longuitud:" +
+      " " +
+      analyzer.getAverageWordLength(textarea.value); // 6 Longitud media de las palabras: la aplicación debe calcular la longitud media de las palabras en el texto
   });
 });
-
